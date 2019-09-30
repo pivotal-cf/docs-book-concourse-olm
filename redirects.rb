2d9ca593e2b5dc3f2b72c67dcd75c861f64a7f62
Chris Wong <cwong@pivotal.io>
2019-09-30 17:37:48 -0400

    redirect to MkDocs version - attempt 4 [#168852509]

--- redirects.rb
+++ redirects.rb
@@ -1,5 +1,5 @@
 #===== Temp Redirect to MkDocs =====#
-r302 %r{docs-pcf-staging.cfapps.io/p-concourse/(?![\d-]+)(.*)}, "docs.pivotal.io/concourse-pcf/v5/"
+r302 %r{https://docs-pcf-staging.cfapps.io/p-concourse/(?![\d-]+)(.*)}, "docs.pivotal.io/concourse-pcf/v5/"
 
 
 
