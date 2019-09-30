361f59d2ee646f8ed8f31701c074812894449295
Chris Wong <cwong@pivotal.io>
2019-09-30 17:14:12 -0400

    redirect to MkDocs version [#168852509]

--- redirects.rb
+++ redirects.rb
@@ -1,7 +1,14 @@
-# Redirect all production http traffic to https
-r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
-  rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
-}
+#===== Temp Redirect to MkDocs =====#
+r302 %r{/p-concourse/(?![\d-]+)(.*)}, "/concourse-pcf/v5/"
 
-r301 %r{/p-concourse/3-0/(.*)}, '/p-concourse/3-x/$1'
-r301 %r{/p-concourse/(?![\d-]+)(.*)}, "/p-concourse/5-x/$1"
+
+
+#===== Original Redirects =====#
+
+# Redirect all production http traffic to https
+# r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
+#   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
+#}
+
+# r301 %r{/p-concourse/(?![\d-]+)(.*)}, "/p-concourse/5-x/$1"
+# r301 %r{/p-concourse/3-0/(.*)}, '/p-concourse/3-x/$1'
