1e76cee1c1e610a2cfaa4dc8e19d8f9de0085726
Chris Wong <cwong@pivotal.io>
2019-11-22 12:39:53 -0500

    Remove redirect to  and edit current redirect to use mkdocs versioning [#169843581]

--- redirects.rb
+++ redirects.rb
@@ -1,14 +1,12 @@
 #===== Temp Redirect to MkDocs =====#
-r302 %r{/p-concourse/(?![\d-]+)(.*)}, "https://docs.pivotal.io/concourse-pcf/"
-
-
+#r302 %r{/p-concourse/(?![\d-]+)(.*)}, "https://docs.pivotal.io/concourse-pcf/"
 
 #===== Original Redirects =====#
 
-# Redirect all production http traffic to https
-# r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
-#   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
-#}
+Redirect all production http traffic to https
+r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
+   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
+}
 
-# r301 %r{/p-concourse/(?![\d-]+)(.*)}, "/p-concourse/5-x/$1"
-# r301 %r{/p-concourse/3-0/(.*)}, '/p-concourse/3-x/$1'
+ r301 %r{/p-concourse/(?![\d-]+)(.*)}, "/p-concourse/v5/$1"
+ r301 %r{/p-concourse/3-0/(.*)}, '/p-concourse/v3/$1'
